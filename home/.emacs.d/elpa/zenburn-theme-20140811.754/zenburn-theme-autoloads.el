58a660bb5ed25546239966b768734327e0cb9454
David Zuber <zuber.david@gmx.de>
2014-09-26 23:22:42 +0200

    emacs configs

---
+++ home/.emacs.d/elpa/zenburn-theme-20140811.754/zenburn-theme-autoloads.el
@@ -0,0 +1,26 @@
+;;; zenburn-theme-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil "zenburn-theme" "zenburn-theme.el" (21541 53846
+;;;;;;  89656 219000))
+;;; Generated autoloads from zenburn-theme.el
+
+(and load-file-name (boundp 'custom-theme-load-path) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+;;;***
+
+;;;### (autoloads nil nil ("zenburn-theme-pkg.el") (21541 53846 97482
+;;;;;;  116000))
+
+;;;***
+
+(provide 'zenburn-theme-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; zenburn-theme-autoloads.el ends here
