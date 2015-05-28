11c57b3ec3bcbacd428a4d92787e09ebfcbb207b
David Zuber <zuber.david@gmx.de>
2015-05-28 11:30:00 +0000

    Add ssh config and create keys scripts

---
+++ home/createkeys.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd ~/.ssh
+
+declare -a hosts=(
+    github
+    bitbucket
+)
+
+for host in "${hosts[@]}"
+do
+    echo "Create key pair for host: ${host}"
+    ssh-keygen -t rsa -b 4096 -C "zuber.david@gmx.de" -f "id_${host}4096_rsa"
+done
