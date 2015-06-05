724da4fa98073539990cd2bbc86c9af21dfac094
David Zuber <zuber.david@gmx.de>
2015-06-05 10:58:08 +0000

    Create keys automatically adds them to bitbucket and github

--- home/createkeys.sh
+++ home/createkeys.sh
@@ -13,3 +13,9 @@ do
     echo "Create key pair for host: ${host}"
     ssh-keygen -t rsa -b 4096 -C "zuber.david@gmx.de" -f "id_${host}4096_rsa"
 done
+
+read -p "Title for public keys: " KEYTITLE
+GITHUBKEY=`cat ~/.ssh/id_github4096_rsa.pub`
+BITBUCKETKEY=`cat ~/.ssh/id_bitbucket4096_rsa.pub`
+http -a storax --json https://api.github.com/user/keys title=$KEYTITLE key=$GITHUBKEY
+http -a dzuber --json https://bitbucket.org/api/1.0/users/dzuber/ssh-keys label=$KEYTITLE key=$BITBUCKETKEY
