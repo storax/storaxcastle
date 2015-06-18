a6ede3db4e5875a23328ccffd29e40b310c7ba16
David Zuber <zuber.david@gmx.de>
2015-06-18 19:55:59 +0000

    Quote keyfile for createkeys.sh

--- home/createkeys.sh
+++ home/createkeys.sh
@@ -17,5 +17,5 @@ done
 read -p "Title for public keys: " KEYTITLE
 GITHUBKEY=`cat ~/.ssh/id_github4096_rsa.pub`
 BITBUCKETKEY=`cat ~/.ssh/id_bitbucket4096_rsa.pub`
-http -a storax --json https://api.github.com/user/keys title=$KEYTITLE key=$GITHUBKEY
-http -a dzuber --json https://bitbucket.org/api/1.0/users/dzuber/ssh-keys label=$KEYTITLE key=$BITBUCKETKEY
+http -a storax --json https://api.github.com/user/keys title="$KEYTITLE" key="$GITHUBKEY"
+http -a dzuber --json https://bitbucket.org/api/1.0/users/dzuber/ssh-keys label="$KEYTITLE" key="$BITBUCKETKEY"
