99b232d2ad1a1958e92073079493b7179550b60e
David Zuber <zuber.david@gmx.de>
2015-12-13 17:10:41 +0100

    Fix desktop autosave

--- home/.emacs.d/lisp/init-desktop.el
+++ home/.emacs.d/lisp/init-desktop.el
@@ -4,7 +4,8 @@
 (defun my-desktop-save ()
   (interactive)
   ;; Don't call desktop-save-in-desktop-dir, as it prints a message.
-  (if (eq (desktop-owner) (emacs-pid))))
+  (if (eq (desktop-owner) (emacs-pid))
+      (desktop-save desktop-dirname)))
 
 (add-hook 'auto-save-hook 'my-desktop-save)
 
