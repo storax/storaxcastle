39fbc862d42ec9769a7176addb4dc9e788bc418f
David Zuber <zuber.david@gmx.de>
2015-12-13 15:04:05 +0100

    Add eval region shortcut in lisp

--- home/.emacs.d/lisp/init-elisp.el
+++ home/.emacs.d/lisp/init-elisp.el
@@ -3,6 +3,18 @@
   (setq imenu-prev-index-position-function nil)
   (add-to-list 'imenu-generic-expression '("Sections" "^;;;; \\(.+\\)$" 1) t))
 
+;; Make C-x C-e run 'eval-region if the region is active
+(defun sanityinc/eval-last-sexp-or-region (prefix)
+  "Eval region from BEG to END if active, otherwise the last sexp."
+  (interactive "P")
+  (if (and (mark) (use-region-p))
+      (eval-region (min (point) (mark)) (max (point) (mark)))
+    (pp-eval-last-sexp prefix)))
+
+(global-set-key (kbd "M-:") 'pp-eval-expression)
+(after-load 'lisp-mode
+  (define-key emacs-lisp-mode-map (kbd "C-x C-e") 'sanityinc/eval-last-sexp-or-region))
+
 (add-hook 'emacs-lisp-mode-hook 'imenu-elisp-sections)
 
 (provide 'init-elisp)
