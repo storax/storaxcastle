ab10b5a8f04e07b501d121bb2775606c4714f0ec
David Zuber <zuber.david@gmx.de>
2015-12-14 22:27:41 +0100

    Return to standard search instead of swoop

--- home/.emacs.d/lisp/init-helm.el
+++ home/.emacs.d/lisp/init-helm.el
@@ -40,8 +40,7 @@
 
 ;; Helm-Swoop
 (require 'helm-swoop)
-(global-set-key (kbd "C-s") 'helm-swoop)
-(global-set-key (kbd "C-r") 'helm-swoop)
+(global-set-key (kbd "M-i") 'helm-swoop)
 (global-set-key (kbd "M-I") 'helm-swoop-back-to-last-point)
 (global-set-key (kbd "C-c M-i") 'helm-multi-swoop)
 (global-set-key (kbd "C-x M-i") 'helm-multi-swoop-all)
