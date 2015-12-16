8980a26299275d8e640b9bda93db54c4cd1785c9
David Zuber <zuber.david@gmx.de>
2015-12-16 06:46:38 +0100

    Diminish projectile mode

--- home/.emacs.d/lisp/init-powerline.el
+++ home/.emacs.d/lisp/init-powerline.el
@@ -103,6 +103,7 @@
 (eval-after-load "highlight-indentation" '(diminish 'highlight-indentation-mode))
 (eval-after-load "highlight-indentation" '(diminish 'highlight-indentation-current-column-mode))
 (eval-after-load "subword" '(diminish 'subword-mode))
+(eval-after-load "projectile" '(diminish 'projectile-mode))
 
 ;;;; Custom theme with flycheck color
 (defun my-powerline-center-theme ()
