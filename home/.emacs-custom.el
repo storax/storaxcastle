95777726bf7177c331aa9fd395d3c80ad89aabf2
David Zuber <zuber.david@gmx.de>
2015-12-16 01:31:40 +0100

    Change scrollbar mode

--- home/.emacs-custom.el
+++ home/.emacs-custom.el
@@ -74,7 +74,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(font-latex-sectioning-5-face ((t (:inherit fixed-pitch :foreground "#CC9393" :weight bold))) t)
+ '(font-latex-sectioning-5-face ((t (:inherit fixed-pitch :foreground "#CC9393" :weight bold))))
  '(powerline-inactive1 ((t (:inherit mode-line-inactive :background "#4F4F4F" :foreground "#7F9F7F"))))
  '(powerline-inactive2 ((t (:inherit mode-line-inactive :background "#6F6F6F" :foreground "#9FC29F"))))
- '(region ((t (:background "#222222")))))
+ '(region ((t (:background "#222222"))))
+ '(scroll-bar ((t (:background "#545450" :foreground "#3F3F3F")))))
