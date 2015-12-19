97196f78e627e8a209c183cba7d89af062f4f6be
David Zuber <zuber.david@gmx.de>
2015-12-19 13:17:30 +0100

    Toggle between test and implementation

--- home/.emacs.d/lisp/init-projectile.el
+++ home/.emacs.d/lisp/init-projectile.el
@@ -2,5 +2,13 @@
 
 (projectile-global-mode)
 (setq projectile-completion-system 'helm)
+(projectile-register-project-type 'python-tox2 '("tox.ini") nil "tox")
+
+(defun projectile-test-prefix (project-type)
+  "Find default test files prefix based on PROJECT-TYPE."
+  (cond
+   ((member project-type '(django python python-tox python-tox2)) "test_")
+   ((member project-type '(lein-midje)) "t_")))
+
 
 (provide 'init-projectile)
