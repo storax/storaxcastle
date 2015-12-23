9e82eb9307a609482bad842fcb7575e205a5dd5b
David Zuber <zuber.david@gmx.de>
2015-12-23 03:21:48 +0100

    Nicer doc

--- home/.emacs.d/lisp/init-magit.el
+++ home/.emacs.d/lisp/init-magit.el
@@ -1,13 +1,17 @@
+;;; init-magit --- Configure magit
+
+;;; Commentary:
+
+;;; Code:
 (require-package 'magit)
 (require-package 'magit-gitflow)
+(require 'magit)
+(require 'magit-gitflow)
 
 ;;Magit
 (global-set-key (kbd "C-x g") 'magit-status)
 
-;; Magit Readme buffer
-(setq magit-last-seen-setup-instructions "1.4.0")
-
-(require 'magit-gitflow)
 (add-hook 'magit-mode-hook 'turn-on-magit-gitflow)
 
 (provide 'init-magit)
+;;; init-magit ends here
