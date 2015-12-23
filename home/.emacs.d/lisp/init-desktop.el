d70676279d35a25da4104e705023a0c715228de8
David Zuber <zuber.david@gmx.de>
2015-12-23 02:08:57 +0100

    Add prefix and nicer doc

--- home/.emacs.d/lisp/init-desktop.el
+++ home/.emacs.d/lisp/init-desktop.el
@@ -1,12 +1,18 @@
-;;;; Save Desktop
+;;; init-desktop --- save desktop on save
+
+;;; Commentary:
+
+;;; Code:
 (require 'desktop)
 (desktop-save-mode 1)
-(defun my-desktop-save ()
+(defun storax/desktop-save ()
+  "Save the current desktop to desktop-dirname."
   (interactive)
   ;; Don't call desktop-save-in-desktop-dir, as it prints a message.
   (if (eq (desktop-owner) (emacs-pid))
       (desktop-save desktop-dirname)))
 
-(add-hook 'auto-save-hook 'my-desktop-save)
+(add-hook 'auto-save-hook 'storax/desktop-save)
 
 (provide 'init-desktop)
+;;; init-desktop.el ends here
