1117effcd3dabde47370ff5bcedda43b6e1685bd
David Zuber <zuber.david@gmx.de>
2015-12-23 03:25:50 +0100

    Nice doc

--- home/.emacs.d/lisp/init-latex.el
+++ home/.emacs.d/lisp/init-latex.el
@@ -1,3 +1,10 @@
+;;; init-latex --- For all those thesis stuff
+
+;;; Commentary:
+
+;; Why not org-mode? Shut up! At least not MS-Word.
+
+;;; Code:
 (require-package 'auctex)
 
 (pdf-tools-install)
@@ -9,23 +16,33 @@
 
 ;;; Key-Bindings
 ;; Latex umlauts
-(defun latex-ae () (interactive) (insert "\\\"a"))
-(defun latex-oe () (interactive) (insert "\\\"o"))
-(defun latex-ue () (interactive) (insert "\\\"u"))
-(defun latex-ss () (interactive) (insert "{\\ss}"))
+(defun latex-ae () "Insert ae."
+  (interactive) (insert "\\\"a"))
+(defun latex-oe () "Insert oe."
+  (interactive) (insert "\\\"o"))
+(defun latex-ue () "Insert ue."
+  (interactive) (insert "\\\"u"))
+(defun latex-ss () "Insert sharp s."
+  (interactive) (insert "{\\ss}"))
 
-(defun latex-bindings-my ()
+(defun storax/apply-latex-bindings ()
+  "Apply latex key bindings.
+
+Why is this a function? And used in a hook?"
   (define-key LaTeX-mode-map (kbd "C-' C-a") 'latex-ae)
   (define-key LaTeX-mode-map (kbd "C-' C-o") 'latex-oe)
   (define-key LaTeX-mode-map (kbd "C-' C-u") 'latex-ue)
   (define-key LaTeX-mode-map (kbd "C-' C-s") 'latex-ss)
 )
 
-;;; Hooks
+;;----------------------------------------------------------------------------
+;; Hooks
+;;----------------------------------------------------------------------------
 (add-hook 'pdf-view-mode-hook 'auto-revert-mode)
 ;; use special hook for tex-default-command, as it is a local variable
 (add-hook 'TeX-mode-hook '(lambda () (setq TeX-command-default "latexmk")))
 (add-hook 'LaTeX-mode-hook 'TeX-source-correlate-mode)
-(add-hook 'LaTeX-mode-hook 'latex-bindings-my)
+(add-hook 'LaTeX-mode-hook 'storax/apply-latex-bindings)
 
 (provide 'init-latex)
+;;; init-latex ends here
