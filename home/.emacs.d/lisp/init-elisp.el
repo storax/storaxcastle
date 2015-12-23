657ab25db397049592df0da15505cf6470ffbea3
David Zuber <zuber.david@gmx.de>
2015-12-23 02:37:56 +0100

    Change prefix and add nicer doc

--- home/.emacs.d/lisp/init-elisp.el
+++ home/.emacs.d/lisp/init-elisp.el
@@ -1,11 +1,19 @@
-;; Display sections in imenu for elisp mode
-(defun imenu-elisp-sections ()
+;;; init-elisp -- Configure elisp mode
+
+;;; Commentary:
+
+;;; Code:
+
+(defun storax/imenu-elisp-sections ()
+  "Set settings to display sections in imenu for elisp mode."
   (setq imenu-prev-index-position-function nil)
   (add-to-list 'imenu-generic-expression '("Sections" "^;;;; \\(.+\\)$" 1) t))
 
 ;; Make C-x C-e run 'eval-region if the region is active
-(defun sanityinc/eval-last-sexp-or-region (prefix)
-  "Eval region from BEG to END if active, otherwise the last sexp."
+(defun storax/eval-last-sexp-or-region (prefix)
+  "Eval region from BEG to END if active, otherwise the last sexp.
+
+PREFIX is used for 'pp-eval-last-sexp.'"
   (interactive "P")
   (if (and (mark) (use-region-p))
       (eval-region (min (point) (mark)) (max (point) (mark)))
@@ -13,8 +21,9 @@
 
 (global-set-key (kbd "M-:") 'pp-eval-expression)
 (after-load 'lisp-mode
-  (define-key emacs-lisp-mode-map (kbd "C-x C-e") 'sanityinc/eval-last-sexp-or-region))
+  (define-key emacs-lisp-mode-map (kbd "C-x C-e") 'storax/eval-last-sexp-or-region))
 
-(add-hook 'emacs-lisp-mode-hook 'imenu-elisp-sections)
+(add-hook 'emacs-lisp-mode-hook 'storax/imenu-elisp-sections)
 
 (provide 'init-elisp)
+;;; init-elisp ends here
