5662ed884e2c4e493b4de6a6edd35c9485934849
David Zuber <zuber.david@gmx.de>
2015-12-25 18:00:02 +0100

    Intelligent Swap function with smartparens' help

--- home/.emacs.d/lisp/init-smartparens.el
+++ home/.emacs.d/lisp/init-smartparens.el
@@ -9,5 +9,56 @@
 (smartparens-global-mode 1)
 (show-smartparens-global-mode 1)
 
+;;----------------------------------------------------------------------------
+;; Custom Functions
+;;----------------------------------------------------------------------------
+
+(defun storax/swap-place-in-region ()
+  "Go to the other end of the current region.
+
+Set a mark before moving.
+If a region is active this acts like swap places."
+  (interactive)
+  (if (region-active-p)
+      (let ((next (if (equal (point) (region-beginning))
+		      (region-end)
+		    (region-beginning))))
+	(set-mark-command nil)
+	(goto-char next)
+	(setq deactivate-mark nil))
+    (storax/swap-place-in-sexp)))
+
+(defun storax/min-offset (pair p)
+  "Get minimum offset to either beginning or end or PAIR from P."
+  (min (abs (- p (getf pair :beg))) (abs (- p (getf pair :end)))))
+
+(defun storax/closer-pair (pair1 pair2 p)
+  "Return one of PAIR1 or PAIR2 which is closer to P."
+  (let ((off1 (storax/min-offset pair1 p))
+	(off2 (storax/min-offset pair2 p)))
+    (if (<= off1 off2)
+	pair1
+      pair2)))
+
+(defun storax/swap-place-in-sexp ()
+  "Go to the other end of the current sexpression."
+  (interactive)
+  (let* ((next (sp-get-paired-expression))
+	 (previous (sp-get-paired-expression t))
+	 (p (point))
+	 (closest (storax/closer-pair next previous p))
+	 (beg (getf closest :beg))
+	 (end (getf closest :end)))
+    (if (> (abs (- p beg)) (abs (- p end)))
+	(if (>= p end)
+	    (goto-char beg)
+	  (goto-char (+ beg 1)))
+      (if (<= p beg)
+	  (goto-char end)
+	(goto-char (- end 1))))))
+
+(global-set-key (kbd "C-M-.") 'storax/swap-place-in-region)
+(global-set-key (kbd "C-M-,") 'storax/swap-place-in-region)
+
 (provide 'init-smartparens)
 ;;; init-smartparens ends here
