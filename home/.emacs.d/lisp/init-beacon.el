2dd73c4068267b393438b3a5b0dfeeab09218182
David Zuber <zuber.david@gmx.de>
2015-12-26 04:15:30 +0100

    Customize beacon

--- home/.emacs.d/lisp/init-beacon.el
+++ home/.emacs.d/lisp/init-beacon.el
@@ -5,7 +5,12 @@
 ;;; Code:
 
 (require-package 'beacon)
+
 (beacon-mode 1)
+(setq beacon-size 30
+      beacon-blink-delay 0.1
+      beacon-blink-duration 0.2
+      beacon-color "sandy brown")
 
 (provide 'init-beacon)
 ;;; init-beacon ends here
