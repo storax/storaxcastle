4ff59c79f902c479e9621e4f9f09b1fe8cd23e09
David Zuber <zuber.david@gmx.de>
2015-12-27 21:15:51 +0100

    Install user sets too

--- home/.emacs.d/lisp/init-helm-dash.el
+++ home/.emacs.d/lisp/init-helm-dash.el
@@ -24,9 +24,13 @@
 				    "PostgreSQL"
 				    "Python_2"
 				    "Python_3"
-				    "Qt"
+				    "Qt_4"
+				    "Qt_5"
 				    "SQLAlchemy"
 				    "Vagrant"))
+
+(defvar storax/user-docsets-to-install '("Emacs"))
+
 
 (defun storax/dash-install-set (docset)
   "Install the DOCSET if it does not exist already."
@@ -37,8 +41,19 @@
 			       "~"))
       (helm-dash-install-docset docset))))
 
+(defun storax/dash-install-user-set (docset)
+  "Install the user DOCSET if it does not exist already."
+  (let ((name-escaped (replace-regexp-in-string "_" " " docset)))
+    (unless (file-exists-p
+	     (expand-file-name (concat (file-name-as-directory ".docsets")
+				       (concat name-escaped ".docset"))
+			       "~"))
+      (helm-dash-install-user-docset docset))))
+
 (dolist (docset storax/docsets-to-install)
   (storax/dash-install-set docset))
+(dolist (docset storax/user-docsets-to-install)
+  (storax/dash-install-user-set docset))
 
 (provide 'init-helm-dash)
 ;;; init-helm-dash ends here
