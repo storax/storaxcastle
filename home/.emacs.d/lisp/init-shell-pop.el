1d533e0352e020e4bf2402fff3eed7d10edadcc5
David Zuber <zuber.david@gmx.de>
2015-12-28 17:56:51 +0100

    Use custom set variabls

--- home/.emacs.d/lisp/init-shell-pop.el
+++ home/.emacs.d/lisp/init-shell-pop.el
@@ -7,12 +7,13 @@
 (require-package 'shell-pop)
 (require 'shell-pop)
 
-(setq shell-pop-shell-type (quote ("multi-term" "*zsh*" (lambda nil (multi-term))))
-      shell-pop-universal-key "<f10>"
-      shell-pop-full-span t
-      shell-pop-window-position "bottom"
-      shell-pop-default-directory nil
-      shell-pop-window-height 60)
+(custom-set-variables
+ '(shell-pop-default-directory nil)
+ '(shell-pop-shell-type (quote ("multi-term" "*zsh*" (lambda nil (multi-term)))))
+ '(shell-pop-universal-key "<f10>")
+ '(shell-pop-window-size 60)
+ '(shell-pop-full-span t)
+ '(shell-pop-window-position "bottom"))
 
 (provide 'init-shell-pop)
 ;;; init-shell-pop ends here
