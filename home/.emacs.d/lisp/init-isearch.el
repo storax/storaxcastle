832efd4be61f3a78c97fdf0114b0ce998d69005c
David Zuber <zuber.david@gmx.de>
2015-12-28 03:34:30 +0100

    Dont end search after kill isearch result

--- home/.emacs.d/lisp/init-isearch.el
+++ home/.emacs.d/lisp/init-isearch.el
@@ -8,8 +8,7 @@
 (defun hack-isearch-kill ()
    "Push current matching string into kill ring."
    (interactive)
-   (kill-new (buffer-substring (point) isearch-other-end))
-   (isearch-done))
+   (kill-new (buffer-substring (point) isearch-other-end)))
 (define-key isearch-mode-map (kbd "M-w") 'hack-isearch-kill)
 
 (provide 'init-isearch)
