ec8ed481551dbd3e07c2efa95ced4904779c067e
David Zuber <zuber.david@gmx.de>
2015-12-30 16:01:05 +0100

    Add better translate functionality

--- home/.emacs.d/lisp/init-translate.el
+++ home/.emacs.d/lisp/init-translate.el
@@ -4,12 +4,35 @@
 
 ;;; Code:
 
-(require 'init-elpa)
-(require-package 'google-translate)
+(require 'init-utils)
 
-(require 'google-translate)
-(require 'google-translate-smooth-ui)
-(global-set-key (kbd "C-c t") 'google-translate-smooth-translate)
+(defun storax/translate--format (key value)
+  "Format the given KEY and VALUE from the dict."
+  (format "%s\n%s"
+   (propertize key 'face '(:inherit 'font-lock-keyword-face))
+   (propertize value 'face '(:inherti 'font-lock-doc-face))))
+
+(defun storax/translate (database query)
+  "Translate with DATABASE the given QUERY."
+  (let* ((output (shell-command-to-string (format "/usr/bin/dict -d %s %s" database query)))
+	 (defs (sanityinc/string-all-matches "\\(From \\(.*?\\):
+
+  \\(.*?\\)
+   \\(.*?\\)
+\\(?:
+\\)*\\)" output 3))
+	 (exps (sanityinc/string-all-matches "\\(From \\(.*?\\):
+
+  \\(.*?\\)
+   \\(.*?\\)
+\\(?:
+\\)*\\)" output 4))
+	 (buf (get-buffer-create "Translation")))
+    (with-current-buffer buf
+      (erase-buffer)
+      (insert (mapconcat 'identity (mapcar* #'storax/translate--format defs exps) "\n\n")))
+    (unless (get-buffer-window buf 0)
+      (pop-to-buffer buf nil t))))
 
 (provide 'init-translate)
 ;;; init-translate ends here
