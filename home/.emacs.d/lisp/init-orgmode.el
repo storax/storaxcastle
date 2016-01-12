ea8dbfe9fab4a504b9adc42bcb4fd059d1ace9e1
David Zuber <zuber.david@gmx.de>
2016-01-12 13:23:00 +0100

    Add notes file and more todo keywords

--- home/.emacs.d/lisp/init-orgmode.el
+++ home/.emacs.d/lisp/init-orgmode.el
@@ -8,6 +8,12 @@
 (require-package 'org)
 (require-package 'org-plus-contrib)
 (require-package 'orgbox)
+(require 'org)
+
+(unless (file-exists-p org-directory)
+  (make-directory org-directory))
+(setq org-default-notes-file (concat org-directory "/notes.org"))
+
 (require 'orgbox)
 
 (provide 'init-orgmode)
