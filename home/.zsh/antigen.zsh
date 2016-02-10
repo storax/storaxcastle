a7a9a1a9b287b2b3e59a7a6acb01a9a9ea80c3e1
David Zuber <zuber.david@gmx.de>
2016-02-10 00:31:15 +0000

    Add more zsh goodies

--- home/.zsh/antigen.zsh
+++ home/.zsh/antigen.zsh
@@ -6,19 +6,29 @@ source ~/antigen.zsh
 antigen use oh-my-zsh
 
 # Bundles from the default repo (robbyrussell's oh-my-zsh).
+antigen bundle colored-man-pages
+antigen bundle colorize
 antigen bundle command-not-found
 antigen bundle common-aliases
+antigen bundle copydir
+antigen bundle copyfile
+antigen bundle cp
 antigen bundle dirhistory
+antigen bundle emacs
 antigen bundle gem
 antigen bundle git
 antigen bundle git-flow
+antigen bundle nyan
+antigen bundle oknowton/zsh-dwim
 antigen bundle pip
+antigen bundle python
 antigen bundle sudo
 antigen bundle vagrant
-antigen-bundle oknowton/zsh-dwim
+antigen bundle z
 
 # Syntax highlighting bundle.
 antigen bundle zsh-users/zsh-syntax-highlighting
+antigen bundle zsh-users/zsh-completions
 
 # Load the theme.
 antigen theme kphoen
