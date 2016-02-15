817188e07a7f64570b2796f1043d08635abe97d3
David Zuber <zuber.david@gmx.de>
2016-02-15 23:37:00 +0000

    Move zsh to zsh-castle

--- home/.zsh/bindkey.zsh
+++
@@ -1,9 +0,0 @@
-# To see the key combo you want to use just do:
-# cat > /dev/null
-# And press it
-
-# Emacs key bindings
-bindkey -e
-
-#Example:
-#bindkey "^K"      kill-whole-line                      # ctrl-k
--- home/.zsh/prm.zsh
+++
@@ -1,2 +0,0 @@
-[ -d ~/.zsh/extensions/prm ] || git clone https://github.com/eivind88/prm.git ~/.zsh/extensions/prm
-alias prm="source $HOME/.zsh/extensions/prm/prm.sh"
